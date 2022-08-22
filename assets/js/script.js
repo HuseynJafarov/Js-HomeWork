36c58d4d9994e81286f8478df94e5c638a8d16e3
HuseynJafarov <huseynnj@code.edu.az>
2022-08-22 04:34:48 +0400

    FactorilNumsCheck

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+
+
+
+// //task 1
+// let n;
+// function Division(n) {
+    
+//     if(n%7==0 && n%3==0){
+//         console.log("3-e ve 7-ye bolunur");
+//     }else{
+//         console.log("Bolunmur");
+//     }
+    
+// }
+// Division(21)   
+
+// //task 2
+// let a;
+// function Factro(a){
+//     let result = 1;
+
+//   for (let i = 1; i <=a; i++) {  
+//        result*=i;
+//   }
+//   console.log(result);
+
+// }
+// Factro(5)
+
+// //task 3
+// let nums = [1,2,3,4,5,7,9]
+// Sum(nums)
+// function Sum(num) {
+//     let squareSum =0;
+//     for (let i = 0; i < num.length; i++) {
+        
+//         if(nums[i]%2==0){
+//           squareSum += Math.pow(num[i],2)
+//         }   
+//     }   
+//     console.log(squareSum);
+// }  
+
+// // task 4
+// let email = "huseyn@cod.e.edu.az";
+// let password ="123456";
+// Check(email,password)
+// function Check(email,password) {
+//     if (email == "huseyn@cod.e.edu.az",password =="12345") {
+//      console.log("True");  
+//     }else{
+//         console.log("False");
+//     }
+// }
+
+// //task 5
+// let numsOdd = [1,2,3,4,5,6.7,8,9,10]
+// Sum(numsOdd);
+// function Sum(num){
+//     oddSum = 0;
+//     for (let i = 0; i < num.length; i++) {
+//         if (numsOdd[i]%2==1) {
+//             oddSum +=num[i]
+//         }
+//     }
+// console.log(oddSum);
+// }
+
+// //task 6
+// let numsEven = [1,2,3,4,5,6,7,8,9,10]
+// Sum(numsEven);
+// function Sum(num){
+//     evenSum = 0;
+//     for (let i = 0; i < num.length; i++) {
+//         if (numsEven[i]%2==0) {
+//             evenSum +=num[i]
+//         }
+//     }
+// console.log(evenSum);
+// }
